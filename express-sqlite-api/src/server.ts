fc79efe32529b505297b492b97d832526b0cb869
Саша Корнієнко <iqura@Sasas-MacBook-Pro.local>
2024-10-08 19:04:32 +0300

    columns

--- express-sqlite-api/src/server.ts
+++ express-sqlite-api/src/server.ts
@@ -350,9 +350,8 @@ app.post(
   ) => {
     const { currentDbId, currentTableId, selectedDbId, selectedTableId } = req.params
 
-    // Step 1: Get current table name
+    // Step 1: Get current table name and columns
     const getCurrentTableQuery = `SELECT name FROM tables WHERE dbId = ? AND id = ?`
-
     db.get(
       getCurrentTableQuery,
       [currentDbId, currentTableId],
@@ -367,68 +366,106 @@ app.post(
           return res.status(404).json({ error: 'Current table not found' })
         }
 
-        // Step 2: Get selected table name
-        const getSelectedTableQuery = `SELECT name FROM tables WHERE dbId = ? AND id = ?`
+        // Get columns of the current table
+        const getCurrentColumnsQuery = `PRAGMA table_info(${currentTableData.name})`
+        db.all(getCurrentColumnsQuery, [], (err, currentColumns: { name: string }[]) => {
+          if (err) {
+            return res
+              .status(500)
+              .json({ error: 'Error retrieving current table columns', details: err.message })
+          }
 
-        db.get(
-          getSelectedTableQuery,
-          [selectedDbId, selectedTableId],
-          (err, selectedTableData: { name: string }) => {
-            if (err) {
-              return res
-                .status(500)
-                .json({ error: 'Error retrieving selected table', details: err.message })
-            }
-
-            if (!selectedTableData) {
-              return res.status(404).json({ error: 'Selected table not found' })
-            }
-
-            // Step 3: Get all rows from the selected table
-            const getRowsQuery = `SELECT * FROM ${selectedTableData.name}`
-
-            db.all(getRowsQuery, [], (err, rows: string[]) => {
+          // Step 2: Get selected table name and columns
+          const getSelectedTableQuery = `SELECT name FROM tables WHERE dbId = ? AND id = ?`
+          db.get(
+            getSelectedTableQuery,
+            [selectedDbId, selectedTableId],
+            (err, selectedTableData: { name: string }) => {
               if (err) {
-                return res.status(500).json({
-                  error: 'Error retrieving rows from selected table',
-                  details: err.message
-                })
+                return res
+                  .status(500)
+                  .json({ error: 'Error retrieving selected table', details: err.message })
               }
 
-              // Step 4: Iterate through rows and insert into the current table
-              const insertPromises = rows.map((row) => {
-                // Create insert query for current table
-                const columns = Object.keys(row)
-                  .map((col) => `"${col}"`)
-                  .join(', ')
-                const values = Object.values(row)
-                  .map((value) => `"${value}"`)
-                  .join(', ')
-                const insertQuery = `INSERT INTO ${currentTableData.name} (${columns}) VALUES (${values})`
+              if (!selectedTableData) {
+                return res.status(404).json({ error: 'Selected table not found' })
+              }
 
-                return new Promise((resolve, reject) => {
-                  db.run(insertQuery, [], (err) => {
-                    if (err) {
-                      return reject(err)
-                    }
-                    resolve(true)
+              // Get columns of the selected table
+              const getSelectedColumnsQuery = `PRAGMA table_info(${selectedTableData.name})`
+              db.all(getSelectedColumnsQuery, [], (err, selectedColumns: { name: string }[]) => {
+                if (err) {
+                  return res.status(500).json({
+                    error: 'Error retrieving selected table columns',
+                    details: err.message
                   })
+                }
+
+                // Step 3: Map current and selected columns
+                const currentColumnNames = currentColumns.map((col) => col.name)
+                const selectedColumnNames = selectedColumns.map((col) => col.name)
+
+                // Step 4: Check for common columns to merge
+                const commonColumns = currentColumnNames
+                  .filter((col) => selectedColumnNames.includes(col))
+                  .filter((data) => data != 'id')
+
+                // Step 5: Get all rows from the selected table
+                const getRowsQuery = `SELECT * FROM ${selectedTableData.name}`
+                db.all(getRowsQuery, [], (err, rows: { [key: string]: unknown }[]) => {
+                  if (err) {
+                    return res.status(500).json({
+                      error: 'Error retrieving rows from selected table',
+                      details: err.message
+                    })
+                  }
+
+                  // Step 6: Insert rows into the current table for common columns
+                  const insertPromises = rows.map((row) => {
+                    // Prepare data for the insert based on common columns
+                    const insertData = {}
+                    commonColumns.forEach((col) => {
+                      //@ts-ignore
+                      insertData[col] = row[col] // Merge only common columns
+                    })
+
+                    // Create insert query for current table
+                    const columns = Object.keys(insertData)
+                      .map((col) => `"${col}"`)
+                      .join(', ')
+                    const values = Object.values(insertData)
+                      .map((value) => `"${value}"`)
+                      .join(', ')
+
+                    console.log('columns', columns)
+                    console.log('values', values)
+                    const insertQuery = `INSERT INTO ${currentTableData.name} (${columns}) VALUES (${values})`
+
+                    return new Promise((resolve, reject) => {
+                      db.run(insertQuery, [], (err) => {
+                        if (err) {
+                          return reject(err)
+                        }
+                        resolve(true)
+                      })
+                    })
+                  })
+
+                  // Step 7: Wait for all inserts to finish
+                  Promise.all(insertPromises)
+                    .then(() => {
+                      return res.status(200).json({ message: 'Tables merged successfully' })
+                    })
+                    .catch((error) => {
+                      return res
+                        .status(500)
+                        .json({ error: 'Error merging tables', details: error.message })
+                    })
                 })
               })
-
-              // Step 5: Wait for all inserts to finish
-              Promise.all(insertPromises)
-                .then(() => {
-                  return res.status(200).json({ message: 'Tables merged successfully' })
-                })
-                .catch((error) => {
-                  return res
-                    .status(500)
-                    .json({ error: 'Error merging tables', details: error.message })
-                })
-            })
-          }
-        )
+            }
+          )
+        })
       }
     )
   }
